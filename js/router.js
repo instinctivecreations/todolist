0a9210841978db2158f0246d3fc68e8cd39afcb9
Damian Chan <damian@instinctivecreations.com>
2013-08-28 12:34:59 +0100

    Active and completed listings work but "All" listings is now broken.

--- js/router.js
+++ js/router.js
@@ -1,11 +1,35 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes
+	this.route('active');
+	this.route('completed');
   });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function () {
     return Todos.todo.find();
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return Todos.todo.filter(function (todo) {
+      if (!todo.get('isCompleted')) { return true; }
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('Todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function(){
+    return Todos.todo.filter(function (todo) {
+      if (todo.get('isCompleted')) { return true; }
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('Todos/index', {controller: controller});
   }
 });
