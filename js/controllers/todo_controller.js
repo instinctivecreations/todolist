5d36f14779e4fd05d930eb2428c55408003db14d
Damian Chan <damian@instinctivecreations.com>
2013-08-28 09:56:23 +0100

    Successfully deletes a model (todo).

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -19,5 +19,10 @@ Todos.TodoController = Ember.ObjectController.extend({
   acceptChanges: function () {
     this.set('isEditing', false);
     this.get('model').save();
-  }
+  },
+  removeTodo: function () {
+    var todo = this.get('model');
+    todo.deleteRecord();
+    todo.save();
+  },
 });
